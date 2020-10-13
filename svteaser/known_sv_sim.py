7f01c988a9740d0e545756c0bfa3ef3f32d1b28b
Joyjit Daw <jdaw@nvidia.com>
2020-10-13 16:22:27 -0400

    [known_sv_sim] added region based SV spiking
    generate regions around known SV locations and spike them into
    ref seq to generate altered seq.

--- svteaser/known_sv_sim.py
+++ svteaser/known_sv_sim.py
@@ -22,7 +22,10 @@ def serialize_contigs_to_fa(contigs, fa_path):
 
 def generate_altered_ref(ref_file, sv_vcf, outdir, copy_unaltered_contigs):
     """
-    Generate altered ref sequence and return.
+    Generate altered ref sequence and return. This spikes in all the variants into a single altered seq
+    and outputs all altered sequences.
+
+    NOTE: Unused right now.
     """
     reference = pysam.FastaFile(ref_file)
 
@@ -91,6 +94,75 @@ def generate_altered_ref(ref_file, sv_vcf, outdir, copy_unaltered_contigs):
         vcf_compress(path)
 
 
+def add_fasta_entry(name, seq, fasta_fh):
+    fasta_fh.write(">{}\n".format(name))
+    fasta_fh.write("{}\n".format(seq))
+    fasta_fh.flush()
+
+def generate_altered_regions(ref_file, sv_vcf, outdir, region_size, max_sv_size, padding=0):
+    """
+    Simulate variants from known SVs by spiking them into reference segments.
+    """
+    logging.info(f"Region size = {region_size}, Max SV Size = {max_sv_size}, Padding = {padding}")
+    reference = pysam.FastaFile(ref_file)
+    sv = pysam.VariantFile(sv_vcf)
+    header = sv.header
+
+    out_ref_path = os.path.join(outdir, "svteaser.ref.fa")
+    out_ref_fh = open(out_ref_path, "w+")
+
+    out_altered_path = os.path.join(outdir, "svteaser.altered.fa")
+    out_altered_fh = open(out_altered_path, "w+")
+
+    # Number bases to flank on either size ov variant
+    flank_size = region_size // 2
+
+    if flank_size - max_sv_size < padding:
+        logging.error(f"Max SV size and required padding are not compatible. Increase region size by {2 * (padding + max_sv_size - flank_size)} or decreases max SV size to {flank_size - padding}.")
+        exit(1)
+
+
+    records = []
+
+    for record in sv:
+        chrom = record.chrom
+        pos = record.pos - 1
+        ref = record.ref
+        alt = record.alts[0]
+
+        chrom_seq = reference.fetch(chrom)
+
+        if abs(len(alt) - len(ref)) > max_sv_size:
+            logging.debug(f"Skip variations longer than {max_sv_size}")
+            continue
+
+        start_pos = max(0, pos - flank_size)
+        end_pos = min(pos + flank_size, len(chrom_seq))
+
+        ref_seq = chrom_seq[start_pos:end_pos]
+        relative_pos = pos - start_pos
+        alt_seq = "".join([ref_seq[:relative_pos], alt, ref_seq[relative_pos + len(ref):]])
+        new_contig_name = f"{chrom}_{start_pos}_{end_pos}"
+
+        header.add_line(f"##contig=<ID={new_contig_name},length={len(ref_seq)}>")
+
+        add_fasta_entry(new_contig_name, ref_seq, out_ref_fh)
+        add_fasta_entry(new_contig_name, alt_seq, out_altered_fh)
+
+        record.chrom = new_contig_name
+        record.pos = relative_pos + 1
+        records.append(record)
+
+    out_altered_fh.close()
+    out_ref_fh.close()
+
+    out_vcf_path = os.path.join(outdir, "svteaser.sim.vcf")
+    with pysam.VariantFile(out_vcf_path, "w", header=sv.header) as out_vcf_fh:
+        for rec in records:
+            out_vcf_fh.write(rec)
+
+    vcf_compress(out_vcf_path)
+
 def known_sv_sim_main(args):
     """
     Run simulation on reference with known SVs. Output them into a directory.
@@ -104,10 +176,12 @@ def known_sv_sim_main(args):
         logging.error(f"Output directory {args.output} already exists")
         exit(1)
 
-    final_contigs = generate_altered_ref(args.reference,
-                                         args.sv_vcf,
-                                         args.output,
-                                         args.copy_unaltered_contigs)
+    generate_altered_regions(args.reference,
+                             args.sv_vcf,
+                             args.output,
+                             args.len_sv_region,
+                             args.max_sv_size,
+                             padding=args.ref_seq_padding)
 
     logging.info("Finished")
 
@@ -127,8 +201,15 @@ def parseArgs(args):
                         help="SVTeaser output basename (%(default)s)")
     parser.add_argument("--debug", action="store_true",
                         help="Verbose logging")
-    parser.add_argument("--copy_unaltered_contigs", action="store_true",
-                        help="Save both altered and UNaltered contigs from reference into altered fasta. Default to save only altered contigs.")
+    parser.add_argument('--len_sv_region', type=int, default=10000,
+                        help='The length of regions to create.',
+                        required=False)
+    parser.add_argument('--max_sv_size', type=int, default=4000,
+                        help='Max length of variations to spike.',
+                        required=False)
+    parser.add_argument('--ref_seq_padding', type=int, default=800,
+                        help='Padded region around each end of reg where variation are spiked.',
+                        required=False)
     args = parser.parse_args(args)
     args.reference = os.path.abspath(args.reference)
     args.output = args.output + ".svt"
